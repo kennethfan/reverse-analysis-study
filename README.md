af87f817242a56024ef7eaad47f29b15bccacf8b
kennethfan <kennethfan137@gmail.com>
2025-08-01 16:01:48 +0800

    day1: call js function from python

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # reverse analysis study note
+
+## steps
+1. day1: call js function from python (python调用js方法)
---
+++ test.js
@@ -0,0 +1,7 @@
+function hello(input) {
+  return 'hello ' + input;
+}
+
+function sum(a, b) {
+  return a + b
+}
